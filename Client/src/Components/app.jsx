f88363158c9205cf27707b17f6ceee6fd3280ce3
Kenny Kha <kennykha@gmail.com>
2020-11-13 14:08:19 -0800

    Added basic grid layout for review components

--- Client/src/Components/app.jsx
+++ Client/src/Components/app.jsx
@@ -2,6 +2,24 @@ import React, { component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ReviewList from './reviewList.jsx';
+import styled from 'styled-components';
+
+const Title = styled.h2`
+  color: Blue;
+  font-size: 30px;
+`;
+
+const Grid = styled.div`
+
+`;
+
+const Row = styled.div`
+  display:flex;
+`;
+
+const Col = styled.div`
+  flex: ${(props) => props.size};
+`;
 
 class App extends React.Component {
   constructor(props) {
@@ -36,8 +54,38 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h3>Review List</h3>
-        <ReviewList reviewState={this.state} onLoadMoreClick={this.onLoadMoreClick}/>
+        <Title>Review List</Title>
+        <Grid>
+          <Row>
+            <Col size={1}>
+                  <Row>
+                    Top left column
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    Top left column
+                  </Row>
+                  <Row>
+                    Bottom left column
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    Bottom left column
+                  </Row>
+            </Col>
+            <Col size={2}>
+              <ReviewList reviewState={this.state} onLoadMoreClick={this.onLoadMoreClick}/>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
