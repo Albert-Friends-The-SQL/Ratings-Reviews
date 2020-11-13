a25a6fa458d02398bb74d2ef22328b1158925ad1
Kenny Kha <kennykha@gmail.com>
2020-11-12 17:22:26 -0800

    Seeding file to generate dummy reviews to the database

---
+++ Database/seeding.js
@@ -0,0 +1,62 @@
+const faker = require('faker/locale/en');
+const db = require('./index');
+// const db = require('./index');
+
+const reviewBuilder = () => {
+  const ratingRange = { min: 1, max: 5 };
+
+  return {
+    user_email: faker.internet.email(),
+    user: faker.internet.userName(),
+    review_title: faker.lorem.sentence(),
+    description: faker.lorem.sentence(),
+    review_date: faker.date.recent(),
+    ratings: {
+      size: faker.random.number(ratingRange),
+      width: faker.random.number(ratingRange),
+      comfort: faker.random.number(ratingRange),
+      quality: faker.random.number(ratingRange),
+      value: faker.random.number(ratingRange),
+    },
+  };
+};
+
+// for (let i = 0; i < 1; i++) {
+const newReview = reviewBuilder();
+db.query(`INSERT INTO users (
+  user,
+  user_email) values (
+    '${newReview.user}',
+    '${newReview.user_email}')`)
+  .then(() => (
+    db.query(`INSERT INTO reviews (
+      product_id,
+      review_title,
+      description,
+      review_date,
+      verified,
+      size,
+      width,
+      comfort,
+      quality,
+      value,
+      helpfulY,
+      helpfulN
+      ) values (
+        1337,
+        '${newReview.review_title}',
+        '${newReview.description}',
+        '${newReview.review_date}',
+        'yes',
+        ${newReview.ratings.size},
+        ${newReview.ratings.width},
+        ${newReview.ratings.comfort},
+        ${newReview.ratings.quality},
+        ${newReview.ratings.value},
+        0,
+        0)`)
+  ))
+  .then(() => (
+    console.log('Successfull put fake data into the MySQL DB')
+  ));
+// }
