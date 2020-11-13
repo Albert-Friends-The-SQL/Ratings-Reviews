2aec14fa718aa4e811da5279e8c8f40bde1bfdab
Kenny Kha <kennykha@gmail.com>
2020-11-13 11:25:22 -0800

    Deleted faker test file

--- fakertest.js
+++
@@ -1,5 +0,0 @@
-const faker = require('faker/locale/en.js');
-
-// console.log(faker.random.words());
-console.log(faker.date.between('2015-01-01', '2015-01-05'));
-console.log(faker.date.recent().toString().split('').slice(0, 15).join(''));
