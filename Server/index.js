a0308f632d7fb382d7fdb2c924e8cb7ddb8c23fe
Kenny Kha <kennykha@gmail.com>
2020-11-13 10:40:03 -0800

    Added api end point to get reviews

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const App = express();
+const db = require('../Database/index.js');
 
-App.use(express.static('Client/Dist'))
+App.use(express.static('Client/Dist'));
 
 App.get('/', (req, res) => (
   console.log('Hi there, you conencted to the server')
-))
+));
 
+App.get('/api/products/1337/reviews', (req, res) => {
+  db.getReviews((err, success) => {
+    if (err) {
+      res.status(404).send('Error retreiving reviews');
+    } else {
+      res.status(200).send(success);
+    }
+  });
+});
 
 App.listen(3003, () => (
   console.log('Listening on port 3003')
 ))
+
+//Built out App.get for reviews - to make pull request in morning
