43fdd22ab6893ab7717e015198683b6934ebf23c
Kenny Kha <kennykha@gmail.com>
2020-11-15 10:50:22 -0800

    Added grid layout within div to place overall rating and total reviews together

--- Client/src/Components/ratingsBreakdown.jsx
+++ Client/src/Components/ratingsBreakdown.jsx
@@ -1,19 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const RatingsBreakdown = (props) => (
-  <div>
-    <div>4.6 RATING WITH # OF REVIEWS</div>
-    <br></br>
-    <div>5 STARS with bar for number of ratings</div>
-    <br></br>
-    <div>4 STARS with bar for number of ratings</div>
-    <br></br>
-    <div>3 STARS with bar for number of ratings</div>
-    <br></br>
-    <div>2 STARS with bar for number of ratings</div>
-    <br></br>
-    <div>1 STARS with bar for number of ratings</div>
-  </div>
+  <div id='ratingsBreakdown'>
+    {/* ★★★★ */}
+    <Grid>
+      <div id='breakdownBox'>
+        <Row>
+          <Col size={1}>
+            <div id='totalrating'><strong>4.6</strong></div>
+          </Col>
+          <Col id='totalReview' size={1}>
+            <Row>★★★★☆</Row>
+            <Row>100 reviews</Row>
+          </Col>
+        </Row>
+      </div>
+    </Grid>
+      <div>
+        <br></br>
+        <div ><strong>RATING BREAKDOWN</strong></div>
+        <div>5 STARS with bar for number of ratings</div>
+        <br></br>
+        <div>4 STARS with bar for number of ratings</div>
+        <br></br>
+        <div>3 STARS with bar for number of ratings</div>
+        <br></br>
+        <div>2 STARS with bar for number of ratings</div>
+        <br></br>
+        <div>1 STARS with bar for number of ratings</div>
+        <br></br>
+        <br></br>
+      </div>
+    </div>
+
 )
+
+const Grid = styled.div`
+
+`;
+
+const Row = styled.div`
+  display:flex;
+`;
+
+const Col = styled.div`
+  flex: ${(props) => props.size};
+`;
+
 
 export default RatingsBreakdown;
