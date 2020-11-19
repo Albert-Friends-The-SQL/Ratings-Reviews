98c81617120634e06cc62c3fcd301e452fecd302
Kenny Kha <kennykha@gmail.com>
2020-11-19 09:47:44 -0800

    Refactored static recommended percentage to dynamic average

--- Client/src/Components/recommended.jsx
+++ Client/src/Components/recommended.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import RecommendedItem from './recommendedItem.jsx'
+import RecommendedItem from './recommendedItem.jsx';
+import { forEach } from 'underscore';
 
 const Recommended = (props) => {
+  console.log('reco props', props)
+  let recommendedCount = 0;
+  let allReviews = 0;
+  forEach(props.reviewState.reviewData, review => {
+    if (review.recommended === 'yes') {
+      recommendedCount+= 1;
+    }
+    allReviews++;
+  })
+
+  let overallPercentage = Math.round((recommendedCount / allReviews) * 100);
+
   const qualities = ['Size', 'Width', 'Comfort', 'Quality'];
   return (
   <div>
-    <div id='recommendedPercent'><strong>96%</strong></div>
-    <div>of customers recommended this product</div>
+    <div
+      id='recommendedPercent'
+      style={{fontFamily: "adineue PRO KZ Bold", fontSize:"45px", height:"40px"}}
+    >
+      <strong>
+        {overallPercentage}%
+      </strong>
+    </div>
+    <div style={{fontFamily:"AdihausDIN", fontSize:"12px"}}>of customers recommended this product</div>
     <br></br>
     {qualities.map((quality) => (
       <RecommendedItem quality={quality} reviewState={props.reviewState}/>
