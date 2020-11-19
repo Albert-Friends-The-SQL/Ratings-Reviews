0657d121374487552a67ffabe6f23df11cf0050e
Kenny Kha <kennykha@gmail.com>
2020-11-19 09:47:08 -0800

    Adjusted in-line styling for various elements

--- Client/src/Components/reviewListItem.jsx
+++ Client/src/Components/reviewListItem.jsx
@@ -12,7 +12,7 @@ const ReviewListItem = (props) => {
           <br></br>
           <tr>
             <Star starReview={props.review}/>
-            <td id='reviewItemDate'>{props.review.review_date}</td>
+            <td id='reviewItemDate' style={{fontFamily:"AdihausDIN", color:"#767677", fontSize:"11px"}}>{props.review.review_date}</td>
             <br></br>
             <br></br>
             <br></br>
@@ -20,30 +20,43 @@ const ReviewListItem = (props) => {
           </tr>
 
           <tr>
-            <th>{props.review.review_title}</th>
+            <th style={{fontFamily:"adineue PRO KZ Bold", fontSize:"18px", width:"500px"}}>{props.review.review_title}</th>
           </tr>
 
           <tr>
-            <td>{props.review.description}</td>
+            <td style={{fontFamily:"AdihausDIN", fontSize: "13px"}}>{props.review.description}</td>
             <br></br>
             <br></br>
           </tr>
 
           <tr>
-            <td style={{fontSize: "11px"}}>I recommend this product</td>
+            <td style={{fontSize: "11px", fontFamily:"AdihausDIN"}}>
+              <span id="checkmark">
+                <div id="checkmark_stem"></div>
+                <div id="checkmark_kick"></div>
+            </span>
+            I recommend this product</td>
           </tr>
           <tr>
-            <td><strong>{props.review.user}</strong> - Verified Purchaser</td>
+            <td style={{fontFamily:"AdihausDIN", fontSize:"12px"}}><strong style={{fontFamily:"adineue PRO KZ Bold"}}>{props.review.user}</strong><strong style={{color:"#767677", fontSize: "11px"}}> - Verified Purchaser</strong></td>
 
           </tr>
           <br></br>
 
-          <tr>
-            <td>Was this review helpful?</td>
-            <TableDataYLabel onClick={(e, id) => props.onHelpfulClickNumber(e, props.review.id)}>Yes</TableDataYLabel>
-            <TableDataY >[{props.review.helpfulY}]</TableDataY>
-            <TableDataNLabel onClick={(e, id) => props.onHelpfulClickNumber(e, props.review.id)}>No</TableDataNLabel>
-            <TableDataN >[{props.review.helpfulN}]</TableDataN>
+          <tr style={{lineHeight:"10px", position:"relative"}}>
+            <td style={{fontFamily:"AdihausDIN", fontSize:"12px", marginBottom:"50px", width:"130px"}}>Was this review helpful?</td>
+            <TableDataYLabel
+              id = 'tableDataYLabel'
+              onClick={(e, id) => props.onHelpfulClickNumber(e, props.review.id)}
+              >Yes
+            </TableDataYLabel>
+            <TableDataY>[{props.review.helpfulY}]</TableDataY>
+            <TableDataNLabel
+              id = 'tableDataYLabel'
+              onClick={(e, id) => props.onHelpfulClickNumber(e, props.review.id)}
+            >No
+            </TableDataNLabel>
+            <TableDataN>[{props.review.helpfulN}]</TableDataN>
             <br></br>
             <br></br>
           </tr>
@@ -54,19 +67,42 @@ const ReviewListItem = (props) => {
 }
 
 const TableDataYLabel = styled.td`
+  font-family: AdihausDIN;
+  font-size: 12px;
   text-decoration: underline;
+  cursor: pointer;
+  position: absolute;
+  left: 420px;
+  margin-top: 2px;
+  width: 20px;
 `;
 
 const TableDataY = styled.td`
-
-`;
+  font-family: AdihausDIN;
+  font-size: 12px;
+  position: absolute;
+  left: 445px;
+  margin-top: 2px;
+  `;
 
 const TableDataNLabel = styled.td`
+  font-family: AdihausDIN;
+  font-size: 12px;
   text-decoration: underline;
+  cursor: pointer;
+  position: absolute;
+  left: 475px;
+  margin-top: 2px;
+  width: 20px;
+  text-align: center;
 `;
 
 const TableDataN = styled.td`
-
+  font-family: AdihausDIN;
+  font-size: 12px;
+  position: absolute;
+  left: 500px;
+  margin-top: 2px;
 `;
 
 export default ReviewListItem;
